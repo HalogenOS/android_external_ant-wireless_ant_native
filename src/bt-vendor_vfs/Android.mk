f5302015b482bf23884579cfe03cf321757e4b17
Simao Gomes Viana <xdevs23@outlook.com>
2018-02-11 21:28:27 +0100

    bt-vendor_vfs: correct msm/sdm check
    Change-Id: I7c9f3d0827382dc9e9dcff22af6eeb8fada4eaff

--- src/bt-vendor_vfs/Android.mk
+++ src/bt-vendor_vfs/Android.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-ifneq ($(call is-qcom-platform),)
+ifneq ($(call is-qcom-hardware),)
 
 include $(CLEAR_VARS)
 
